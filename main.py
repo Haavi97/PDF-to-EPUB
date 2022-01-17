b1164a55dbe9fafa286444e800150e9f86cf3c2f
Haavi <javierortincervera@gmail.com>
2022-01-17 20:37:07 +0200

    PDFs to text more or less ok

--- main.py
+++ main.py
@@ -3,6 +3,16 @@ import os
 
 from PyPDF2 import PdfFileReader
 
+from pdfminer.pdfdocument import PDFDocument
+from pdfminer.pdfparser import PDFParser
+from pdfminer.pdfinterp import PDFResourceManager, PDFPageInterpreter
+from pdfminer.pdfdevice import PDFDevice, TagExtractor
+from pdfminer.pdfpage import PDFPage
+from pdfminer.converter import XMLConverter, HTMLConverter, TextConverter
+from pdfminer.cmapdb import CMapDB
+from pdfminer.layout import LAParams
+from pdfminer.image import ImageWriter
+
 if not sys.warnoptions:
     import warnings
     warnings.simplefilter("ignore")
@@ -54,12 +64,71 @@ def get_all_raw_text(pdf_path):
 
 
 def get_pdfs():
-    return filter(lambda f: f[-4:] == '.pdf',   os.listdir())
+    cwd = os.getcwd()
+    os.chdir(cwd + os.sep + 'pdfs')
+    pdfs = filter(lambda f: f[-4:] == '.pdf',   os.listdir())
+    os.chdir(cwd)
+    return pdfs
+
+
+def default_folders():
+    current = os.getcwd() + os.sep + 'pdfs'
+    os.mkdir(current) if not os.path.isdir(current) else ''
+    current = os.getcwd() + os.sep + 'texts'
+    os.mkdir(current) if not os.path.isdir(current) else ''
+
+
+def pdf2text(fn, output):
+    default_folders()
+    caching = True
+    rsrcmgr = PDFResourceManager(caching=caching)
+    laparams = LAParams()
+    laparams.all_texts = True
+    imagewriter = None
+    outfp = open('texts' + os.sep + output, 'w')
+    device = TextConverter(rsrcmgr, outfp, laparams=laparams,
+                           imagewriter=imagewriter)
+    with open(fn, 'rb') as fp:
+        interpreter = PDFPageInterpreter(rsrcmgr, device)
+        pagenos = set()
+        maxpages = 0
+        password = b''
+        rotation = 0
+        for page in PDFPage.get_pages(fp, pagenos,
+                                      maxpages=maxpages, password=password,
+                                      caching=caching, check_extractable=True):
+            page.rotate = (page.rotate+rotation) % 360
+            interpreter.process_page(page)
+    device.close()
+    outfp.close()
+
+def clean_texts(fn):
+    with open(fn, 'r') as fi:
+        with open(fn[:-4] + '_cleaned.txt', 'w') as fo:
+            prev = ''
+            for l in fi.readlines():
+                line = l.strip()
+                if line == '':
+                    line = '\n'
+                if len(prev) < 70:
+                    line += '\n'
+                fo.write(line)
+                prev = line
+
 
 
 if __name__ == '__main__':
+    # try:
     for path in get_pdfs():
         print(path)
-        extract_information(path)
-        print(get_page_content(path, 0))
+        fo = path[:-4] + '.txt'
+        pdf2text('pdfs' + os.sep + path, fo)
+        cwd = os.getcwd()
+        os.chdir(cwd + os.sep + 'texts')
+        clean_texts(path[:-4] + '.txt')
+        os.chdir(cwd)
+        # extract_information(path)
+        # print(get_page_text(path, 3))
         # get_all_raw_text(path)
+    # except:
+    #     print('No pdfs')
